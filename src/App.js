8c2bbb0fcfa390d7b3f1ea9802619f7602c23bb7
nikita2828 <nikitavoskoboinikov2828@gmail.com>
2021-01-11 12:01:15 +0200

    start the project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
 function App() {
-  const [names, setName] = useState();
-  useEffect(() => {
-    axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
-      setName(res.data);
-    });
-  }, []);
-  console.log(names);
-  return names ? (
-    <div>
-      <ul>
-        {names.map((name) => (
-          <li key={name.id}>{name.name}</li>
-        ))}
-      </ul>
-    </div>
-  ) : null;
+  return <div></div>;
 }
 
 export default App;
