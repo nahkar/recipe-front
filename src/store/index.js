5f76390f1bcd94b620080d35690ebf1689d4bdc0
nikita2828 <nikitavoskoboinikov2828@gmail.com>
2021-01-11 16:47:49 +0200

    added store folder with redux

---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+
+const rootReducer = combineReducers({});
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+export default store;
