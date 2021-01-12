9d5802054ffd8a3f8c184bcf06fd7470cb7531ab
nikita2828 <nikitavoskoboinikov2828@gmail.com>
2021-01-12 12:16:58 +0200

    added styled

--- src/Registration/index.js
+++ src/Registration/index.js
@@ -15,19 +15,20 @@ export default function Registration() {
     <RegistrationStyled.WrapperForRegistration>
       <form onSubmit={handleSubmit(onSubmit)}>
         <RegistrationStyled.Heading>Email</RegistrationStyled.Heading>
-        <input
+        <RegistrationStyled.Input
           type="text"
           name="email"
           id=""
           ref={register({ required: true, minLength: 5, maxLength: 100 })}
         />
+
         {errors.email && (
           <RegistrationStyled.Error>
             Email must have at least 5 characters
           </RegistrationStyled.Error>
         )}
         <RegistrationStyled.Heading>Password</RegistrationStyled.Heading>
-        <input
+        <RegistrationStyled.Input
           name="password"
           type="password"
           ref={register({
@@ -38,14 +39,14 @@ export default function Registration() {
             },
           })}
         />
+
         {errors.password && (
           <RegistrationStyled.Error>
             {errors.password.message}
           </RegistrationStyled.Error>
         )}
         <RegistrationStyled.Heading>Repeat password</RegistrationStyled.Heading>
-
-        <input
+        <RegistrationStyled.Input
           name="password_repeat"
           type="password"
           ref={register({
@@ -53,13 +54,14 @@ export default function Registration() {
               value === password.current || "The passwords do not match",
           })}
         />
+
         {errors.password_repeat && (
           <RegistrationStyled.Error>
             {errors.password_repeat.message}
           </RegistrationStyled.Error>
         )}
         <RegistrationStyled.WrapperForButton>
-          <input type="submit" />
+          <RegistrationStyled.BtnSubmit type="submit" />
         </RegistrationStyled.WrapperForButton>
       </form>
     </RegistrationStyled.WrapperForRegistration>
