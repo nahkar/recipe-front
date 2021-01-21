fe648b29efeeedcf8b27b616ff150b73f401a394
Andrey <andrei.positko.gb@gmail.com>
2021-01-21 14:09:53 +0200

    create routes for admin panel

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,11 +1,11 @@
 const routes = {
-  login: "/login",
-  registration: "/registration",
-  forgotPass: "/forgotpass",
-  main: "/",
-  notFound: "*",
-  header: "/header",
-  adminpanel: "/adminpanel",
+  login: '/login',
+  registration: '/registration',
+  forgotPass: '/forgotpass',
+  notFound: '*',
+  header: '/header',
+  adminpanel: '/adminpanel',
+  // main: '/',
 };
 
 export default routes;
