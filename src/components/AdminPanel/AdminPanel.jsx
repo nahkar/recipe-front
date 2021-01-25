71a25820ce5b2b51c1a2c9d8e633f56d17370a88
nikita2828 <nikitavoskoboinikov2828@gmail.com>
2021-01-25 17:14:25 +0200

    changed edit btn

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-
-
 import AdminPage  from "./styled";
 import AdminNavigation from "./components/AdminNavigation";
 import Users from "./components/Users";
 import AdminHeader from "./components/AdminHeader"
 export default function AdminPanel() {
   
- 
-
-  
-
   return (
     <AdminPage.Wrapper>
       <AdminNavigation />
